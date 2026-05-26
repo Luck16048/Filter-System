17b98682ebb8bf5f23e971e7ae133291a75a53f7
Luck16048 <karinaparhomenko2007@gmail.com>
2026-05-26 12:00:09 +0200

    feat : add package entity - nationality, sex and person classes

---
+++ src/main/java/org/example/entity/Person.java
@@ -0,0 +1,31 @@
+package org.example.entity;
+
+public class Person {
+    private String name;
+    private int age;
+    private Sex sex;
+    private Nationality nationality;
+
+    public Person(String name, int age, Sex sex, Nationality nationality) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.nationality = nationality;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public Sex getSex() {
+        return this.sex;
+    }
+
+    public Nationality getNationality() {
+        return this.nationality;
+    }
+}
